a152ab35c4dc11a887e696d338a4c4c20c0bf935
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-04 17:56:59 -0700

    Connected tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    mode: 'jit',
+    purge: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
