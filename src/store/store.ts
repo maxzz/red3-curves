007987bb03b07224620720942ee3b4f516ebc50a
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-08 14:32:57 -0700

    WIP. old code separated curve definitions

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
 import { atom } from 'jotai';
-import { InputData } from './components/OldLinePlayground';
+import { InputData } from '../components/OldLinePlayground';
 
 export const pointsAtom = atom<[number, number][]>([[46, 179], [123, 404], [123, 56], [292, 56], [292, 274], [456, 163], [463, 473]]); // control points coordinates as [x, y][]
 
@@ -10,4 +10,4 @@ export const inputDataAtom = atom<InputData>(
         points: get(pointsAtom),
         active: get(nActiveAtom),
     })
-)
+);
