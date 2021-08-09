e43fe72bece6edf3b34d57028960c87a8e3986ab
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-09 03:08:50 -0700

    WIP. new editor. pointer handler. 1

--- src/components/LinePlayground.tsx
+++ src/components/LinePlayground.tsx
@@ -5,29 +5,7 @@ import { colorScale, lineCheckAtom, LineData, linePathesAtom, linesAtom, pointsA
 import { styled } from '@stitches/react';
 import { CURVEINFO } from '../store/datum';
 import { useDrag } from 'react-use-gesture';
-
-function pointer(event: React.PointerEvent<Element> | PointerEvent, node?: HTMLElement | SVGElement | null): [number, number] {
-    if (node === undefined) node = event.currentTarget as any;
-    if (node) {
-        var svg = ((node as any).ownerSVGElement || node) as SVGSVGElement;
-        if (svg.createSVGPoint) {
-            var point = svg.createSVGPoint();
-            point.x = event.clientX, point.y = event.clientY;
-            point = point.matrixTransform((node as any as SVGGraphicsElement).getScreenCTM()?.inverse());
-            
-            //console.log('svg', node, svg, point, (node as any as SVGGraphicsElement).getScreenCTM()?.inverse());
-            //console.log('svg', point);
-
-            return [point.x, point.y];
-        }
-        if (node.getBoundingClientRect) {
-            var rect = node.getBoundingClientRect();
-            return [event.clientX - rect.left - node.clientLeft, event.clientY - rect.top - node.clientTop];
-        }
-    }
-    return [event.pageX, event.pageY];
-}
-
+import { pointer } from '../hooks/pointer';
 
 const LinePointRow = styled('circle', {
     fill: '#00d7ff5a',
---
+++ src/hooks/pointer.ts
@@ -0,0 +1,21 @@
+export function pointer(event: React.PointerEvent<Element> | PointerEvent, node?: HTMLElement | SVGElement | null): [number, number] {
+    if (node === undefined) node = event.currentTarget as any;
+    if (node) {
+        var svg = ((node as any).ownerSVGElement || node) as SVGSVGElement;
+        if (svg.createSVGPoint) {
+            var point = svg.createSVGPoint();
+            point.x = event.clientX, point.y = event.clientY;
+            point = point.matrixTransform((node as any as SVGGraphicsElement).getScreenCTM()?.inverse());
+            
+            //console.log('svg', node, svg, point, (node as any as SVGGraphicsElement).getScreenCTM()?.inverse());
+            //console.log('svg', point);
+
+            return [point.x, point.y];
+        }
+        if (node.getBoundingClientRect) {
+            var rect = node.getBoundingClientRect();
+            return [event.clientX - rect.left - node.clientLeft, event.clientY - rect.top - node.clientTop];
+        }
+    }
+    return [event.pageX, event.pageY];
+}
