f004297c587c5cbd68068f4bd721f111949f9800
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-11 02:05:12 -0700

    WIP. new editor. test wwayne react-tooltip. connected

--- src/components/LinePlayground.tsx
+++ src/components/LinePlayground.tsx
@@ -213,7 +213,7 @@ function MenuCheckboxRow({ line, idx }: { line: LineData, idx: number; }) {
                 <div className="ml-2">{CURVEINFO[idx].name}</div>
             </div>
             {/* Info icon */}
-            <div className="text-gray-600">
+            <div className="text-gray-600" data-tip="">
                 <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
