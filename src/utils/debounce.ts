4896a79f940abdb7c1e737269693b9f072cad469
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-12 00:23:34 -0700

    WIP. update hook and debounce types. 1.

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,10 +1,10 @@
-export default function debounce(f: Function, timeout = 100) {
+export default function debounce<T extends Function>(f: T, timeout = 100) {
     let timer: any;
     let lastArgs: any;
     let lastThis: any;
 
-    return function (...args: any[]) {
-        //@ts-ignore
+    return function (this: any, ...args: any[]) {
+        ////@ts-ignore
         lastThis = this;
         lastArgs = args;
         if (timer) {
