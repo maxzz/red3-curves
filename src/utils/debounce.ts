40898fad3fe81f47eac0e4ce0c23ba6b46efb131
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-12 00:50:59 -0700

    WIP. update hook and debounce types. 3. fixed

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,25 +1,9 @@
-// If it is a function, return the original value directly
-type ObjectKeysPartial<T> = T extends (...args: any[]) => any ? T : Partial<T>
-
-// If the original value of the function is returned, it is not a function. Change the value of each key to optional, but the function returns the original array
-type ObjectValuesPartial<T> = T extends (...args: any[]) => any
-  ? T
-  : {
-      [K in keyof T]: ObjectKeysPartial<T[K]>
-    }
-
-type FunctionParamsValuePartial<T> = T extends (...args: infer P) => infer R
-  ? (...args: ObjectValuesPartial<P>) => R
-  : T
-
-export default function debounce<T extends Function>(f: T, timeout = 100) {
+export default function debounce<T extends (...args: any[]) => any>(f: T, timeout = 100) {
     let timer: any;
     let lastArgs: any;
     let lastThis: any;
 
-    type Fn = FunctionParamsValuePartial<T>;
-
-    return function (this: any, ...args: any[]) {
+    return function (this: any, ...args: Parameters<T>) {
         ////@ts-ignore
         lastThis = this;
         lastArgs = args;
