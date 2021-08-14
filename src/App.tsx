5df65b38f214fd4b4a296fc276ed2f24d7c4e8da
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-14 02:41:18 -0700

    WIP. App. add simple logo

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import LinePlayground from './components/LinePlayground';
 import OldLinePlayground from './components/OldLinePlayground';
+import logoImage from './assets/app-logo.svg';
 
 function App() {
     return (
         <div className="App">
+            <div className="">
+                <img src={logoImage} alt="logo" />
+            </div>
             <LinePlayground />
             {/* <OldLinePlayground /> */}
         </div>
