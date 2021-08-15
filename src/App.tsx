0d56982826878c0483ce6879200e609e5b1a0243
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-14 20:45:30 -0700

    WIP. App. fix react compalins on custom css attrs

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,37 @@ import OldLinePlayground from './components/OldLinePlayground';
 import logoImage from './assets/app-logo.svg';
 
 function App() {
+    // const borderColor = '#8c01ff7a';
+    const borderColor = '#8e34eb7a';
     return (
         <div className="App">
             <div className="w-full h-64 flex items-center">
-                <img className="bg-yellow-800" src={logoImage} alt="logo" />
-                {/* <div className="flex-1 self-center text-4xl font-extrabold">D3 cusrves</div> */}
-                <div className="w-full h-full flex place-items-end bg-yellow-700">
-                    {/* preserveAspectRatio="none" */}
-                    <svg className="bg-red-300" viewBox="0 0 1800 100" preserveAspectRatio="xMinYMid">
-                        <g>
-                        {/* <g style={{transform: 'translate(0px, 50%)'}}> */}
-                        {/* <g style={{transform: 'scale(1) translate(0px, 50px)'}}> */}
-                            <text>
-                                <tspan className="text-[96px] font-black uppercase" fill="none" stroke="red" x={0} y={84}>D3 Curves</tspan>
-                                {/* <tspan className="text-[96px] font-black uppercase" fill="none" stroke="red" x={0} y={84}>Data-Driven Documents: Curves</tspan> */}
-                                {/* <tspan className="text-[96px] font-black uppercase" fill="none" stroke="red" x={0} y={'48'}>Data-Driven Documents: Curves</tspan> */}
-                            </text>
-                        </g>
-                    </svg>
+                <img className="" src={logoImage} alt="logo" />
+                <div className="text-[5rem] tracking-tighter font-black "
+                    style={{//bg-yellow-700 //bg-yellow-800
 
-                    {/* <svg className="w-full h-56 bg-purple-100" viewBox="0 0 300 300" >
-                        <text>
-                            <tspan className="text-[7rem]" fill="black" x={0} y={'60%'}>Data Driven Document Curves</tspan>
-                        </text>
-                    </svg> */}
+                        color: 'white', //#9494e4
 
-                    {/* <tspan className="text-4xl" fill="black" x={0} y={'70%'}>D3 curves</tspan> */}
+                        textShadow:
+                            `
+                            -2px -2px 0 ${borderColor},
+                            2px -2px 0 ${borderColor},
+                            -2px 2px 0 ${borderColor},
+                            2px 2px 0 ${borderColor}
+                            `,
+
+                        '-webkit-text-stroke': '1px #8c01ff',
+                        '-webkit-text-fill-color': '#9494e4'
+                    } as React.CSSProperties}
+                >
+                    D3 Curves
                 </div>
             </div>
             <LinePlayground />
-            {/* <OldLinePlayground /> */}
-        </div>
+            {/* <OldLinePlayground /> */ }
+        </div >
     );
 }
 
 export default App;
+
