fd61bd1e87ff10ab3b0bb01f9f9ecdf564b25667
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-14 17:05:23 -0700

    WIP. App. svg free stretching

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,17 @@ import logoImage from './assets/app-logo.svg';
 function App() {
     return (
         <div className="App">
-            <div className="flex">
-                <img src={logoImage} alt="logo" />
-                <div className="flex-1 self-center text-4xl font-extrabold">D3 cusrves</div>
-                <svg viewBox="0 0 300 300">
-                    <text>
-                        <tspan className="text-[2.5rem]" fill="black" x={0} y={'50%'}>D3 curves</tspan>
-                        <tspan className="text-4xl" fill="black" x={0} y={'70%'}>D3 curves</tspan>
-                    </text>
-                </svg>
+            <div className="w-full h-64 flex items-center">
+                <img className="bg-yellow-800" src={logoImage} alt="logo" />
+                {/* <div className="flex-1 self-center text-4xl font-extrabold">D3 cusrves</div> */}
+                <div className="h-full w-full bg-yellow-700">
+                    <svg className="" viewBox="0 0 300 300" preserveAspectRatio="none">
+                        <text>
+                            <tspan className="text-[20]" fill="black" x={0} y={'50'}>D3 curves</tspan>
+                            {/* <tspan className="text-4xl" fill="black" x={0} y={'70%'}>D3 curves</tspan> */}
+                        </text>
+                    </svg>
+                </div>
             </div>
             <LinePlayground />
             {/* <OldLinePlayground /> */}
