24b4a3b537694a29f21cfe58b37873e6fb0512c5
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-14 20:53:37 -0700

    WIP. App. update layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,35 +4,41 @@ import LinePlayground from './components/LinePlayground';
 import OldLinePlayground from './components/OldLinePlayground';
 import logoImage from './assets/app-logo.svg';
 
-function App() {
+function Header() {
     // const borderColor = '#8c01ff7a';
     const borderColor = '#8e34eb7a';
     return (
-        <div className="App">
-            <div className="w-full h-64 flex items-center">
-                <img className="" src={logoImage} alt="logo" />
-                <div className="text-[5rem] tracking-tighter font-black "
-                    style={{//bg-yellow-700 //bg-yellow-800
+        <div className="w-full h-64 flex items-center ">
+            <img className="" src={logoImage} alt="logo" />
+            <div className="text-[5rem] tracking-tighter font-black "
+                style={{//bg-yellow-700 //bg-yellow-800
 
-                        color: 'white', //#9494e4
+                    color: 'white', //#9494e4
 
-                        textShadow:
-                            `
-                            -2px -2px 0 ${borderColor},
-                            2px -2px 0 ${borderColor},
-                            -2px 2px 0 ${borderColor},
-                            2px 2px 0 ${borderColor}
-                            `,
+                    textShadow:
+                        `
+            -2px -2px 0 ${borderColor},
+            2px -2px 0 ${borderColor},
+            -2px 2px 0 ${borderColor},
+            2px 2px 0 ${borderColor}
+            `,
 
-                        '-webkit-text-stroke': '1px #8c01ff',
-                        '-webkit-text-fill-color': '#9494e4'
-                    } as React.CSSProperties}
-                >
-                    D3 Curves
-                </div>
+                    '-webkit-text-stroke': '1px #8c01ff',
+                    '-webkit-text-fill-color': '#9494e4'
+                } as React.CSSProperties}
+            >
+                D3 Curves
             </div>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <div className="App bg-yellow-50">
+            <Header />
             <LinePlayground />
-            {/* <OldLinePlayground /> */ }
+            {/* <OldLinePlayground /> */}
         </div >
     );
 }
--- src/components/LinePlayground.tsx
+++ src/components/LinePlayground.tsx
@@ -348,7 +348,8 @@ function HintTooltip() {
 
 function LinePlayground() {
     return (
-        <div className="bg-purple-100">
+        <div className="">
+            {/* bg-purple-100 */}
             <div className="flex-col items-center hidden sm:flex">
                 <InfoPanelStatic />
             </div>
