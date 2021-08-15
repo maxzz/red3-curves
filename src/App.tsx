5acc4f2a4e3475123e1c3745de390345a2e3f8e4
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-14 18:02:37 -0700

    WIP. App. svg scaling text

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,23 @@ function App() {
             <div className="w-full h-64 flex items-center">
                 <img className="bg-yellow-800" src={logoImage} alt="logo" />
                 {/* <div className="flex-1 self-center text-4xl font-extrabold">D3 cusrves</div> */}
-                <div className="h-full w-full bg-yellow-700">
-                    <svg className="" viewBox="0 0 300 300" preserveAspectRatio="none">
-                        <text>
-                            <tspan className="text-[20]" fill="black" x={0} y={'50'}>D3 curves</tspan>
-                            {/* <tspan className="text-4xl" fill="black" x={0} y={'70%'}>D3 curves</tspan> */}
-                        </text>
+                <div className="w-full h-full flex place-items-end bg-yellow-700">
+                    {/* preserveAspectRatio="none" */}
+                    <svg className="h-[96px] bg-red-300" viewBox="0 0 500 96" preserveAspectRatio="none">
+                        <g style={{transform: 'scale(.3) translate(0px, 50px)'}}>
+                            <text>
+                                <tspan className="text-[96px] uppercase" fill="none" stroke="red" x={0} y={'30%'}>Data-Driven Documents Curves</tspan>
+                            </text>
+                        </g>
                     </svg>
+
+                    {/* <svg className="w-full h-56 bg-purple-100" viewBox="0 0 300 300" >
+                        <text>
+                            <tspan className="text-[7rem]" fill="black" x={0} y={'60%'}>Data Driven Document Curves</tspan>
+                        </text>
+                    </svg> */}
+
+                    {/* <tspan className="text-4xl" fill="black" x={0} y={'70%'}>D3 curves</tspan> */}
                 </div>
             </div>
             <LinePlayground />
