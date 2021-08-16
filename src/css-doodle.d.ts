63469b6592dd3cb91b84ed1e8582f579d43866cc
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-16 15:18:38 -0700

    WIP. css-doodle, remove css-doodle and component definition

--- src/css-doodle.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            'css-doodle': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
-        }
-    }
-}
