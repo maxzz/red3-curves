cfcb600085643ffa0faf0af13e15ad8fe10742e5
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-16 18:17:03 -0700

    WIP. css-doodle, add curves doodle

--- src/components/HeroAttraction.tsx
+++ src/components/HeroAttraction.tsx
@@ -7,22 +7,36 @@ const gridGap = "2";
 const background = () => "red";
 
 const FancyDoodle = doodle`
-    :doodle {
-        @grid: 8 / 90%;
-        @shape: circle;
-    }
-    
-    transition: .2s @r(.6s);
-    border-radius: @pick(20% 0, 0 20%);
-    
-    will-change: transform;
-    transform: scale(@r(.25, 1.25));
-    
-    background: hsla(
-        calc(240 - 6 * @x * @y),
-        70%, 68%, @r.8
+    @grid: 50x1 / 100%;
+
+    @place-cell: center;
+    @size: calc(75% / @I * @i);
+
+    transform: rotate(calc(@i * 5deg));
+
+    border-radius: 30%;
+    border: 1px solid hsla(
+    calc(10 + 4 * @i), 70%, 68%, @r.8
     );
 `;
+
+// const FancyDoodle = doodle`
+//     :doodle {
+//         @grid: 8 / 90%;
+//         @shape: circle;
+//     }
+  
+//     transition: .2s @r(.6s);
+//     border-radius: @pick(20% 0, 0 20%);
+    
+//     will-change: transform;
+//     transform: scale(@r(.25, 1.25));
+    
+//     background: hsla(
+//         calc(240 - 6 * @x * @y),
+//         70%, 68%, @r.8
+//     );
+// `;
 
 // const FancyDoodle = doodle`
 //   :doodle {
@@ -34,7 +48,7 @@ const FancyDoodle = doodle`
 
 export default function HeroAttraction() {
     return (
-        <div className="w-64 h-64">
+        <div className="w-64 h-64 flex-1">
             <FancyDoodle />
         </div>
     );
