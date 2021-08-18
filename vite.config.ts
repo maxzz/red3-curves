d694e4c381bcf58761545b83c0cf7d8ac586bcb0
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-18 15:54:41 -0700

    Feature. Add bundle build vizualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import visualizer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
-    plugins: [reactRefresh()]
+    plugins: [
+        reactRefresh(),
+        visualizer({ template: 'network' }),
+    ]
 });
