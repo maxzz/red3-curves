6f1f57a2ed5c5befccca3c7910705061f43c2527
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-19 00:55:22 -0700

    WIP. Colors schema. enlarge viewer buttons

--- src/components/LinePlayground.tsx
+++ src/components/LinePlayground.tsx
@@ -299,12 +299,12 @@ function PathInfo({ expanded }: { expanded: boolean; }) {
 function DarkLightIcon({ dark }: { dark: boolean; }) {
     return (dark
         ?
-        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
         </svg>
         :
-        <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+        <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
         </svg>
     );
 }
@@ -320,7 +320,7 @@ function InfoPanel() {
             <div className="flex items-center space-x-1">
                 {/* - */}
                 <div
-                    className={`w-4 h-4 pb-1 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
+                    className={`w-6 h-6 pb-1 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
                         flex items-center justify-center
                         ${nActive > 2 ? 'opacity-1' : 'opacity-50'}
                         `}
@@ -331,7 +331,7 @@ function InfoPanel() {
                 >-</div>
                 {/* + */}
                 <div
-                    className={`w-4 h-4 pb-1 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
+                    className={`w-6 h-6 pb-1 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
                         flex items-center justify-center
                         ${nActive < maxNPoints ? 'opacity-1' : 'opacity-50'}
                         `}
@@ -340,7 +340,7 @@ function InfoPanel() {
                 >+</div>
                 {/* Color mode */}
                 <div
-                    className={`w-4 h-4 pb-1 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
+                    className={`w-6 h-6 pb-0.5 text-green-900 border bg-green-200 border-green-600 rounded shadow cursor-pointer select-none 
                         flex items-center justify-center
                         `}
                     title="Dark/Light mode"
@@ -348,7 +348,7 @@ function InfoPanel() {
                 ><DarkLightIcon dark={colorMode} /></div>
                 {/* Info bar */}
                 <div 
-                    className="w-4 h-4 text-green-900 bg-green-200 border border-green-600 rounded shadow cursor-pointer select-none"
+                    className="w-6 h-6 text-green-900 bg-green-200 border border-green-600 rounded shadow cursor-pointer select-none"
                     title="Show/Hide the coordinates of points"
                     onClick={() => setExpanded((prev) => !prev)}
                 >
