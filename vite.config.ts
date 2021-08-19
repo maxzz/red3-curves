df161a1c721b0d05c10822db1ea515995f9b81d2
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-18 17:34:56 -0700

    Bug. Vite intentionally blocks svg inlining

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import { visualizer } from 'rollup-plugin-visualizer';
+import url from '@rollup/plugin-url';
+import image from '@rollup/plugin-image';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '',
     plugins: [
         reactRefresh(),
+        image(),
+        url(
+        //     {
+        //     include: ['**/*.svg'],
+        //     limit: 15000,
+        // }
+        ),
         visualizer({
             filename: 'vizualization.html',
             template: 'sunburst', // sunburst - d3 style (good as default as well); treemap - table (default); network - graph (slow to open).
