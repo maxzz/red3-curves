88b19e9f67cecab4dcd6751a3ff39c9f4e134a7b
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-05 02:28:02 -0700

    WIP. components split. add Section1_Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,9 @@
 import React from 'react';
-import logoImage from './assets/app-logo.svg';
-import { css } from '@stitches/react';
-import HeroAttraction from './components/UI/HeroAttractionion';
 import { Section2_Main } from '@/components/Section2_Main';
 import './App.css';
 // import AppBackground from './assets/bkg/pattern.svg';
 import AppBackground from './assets/bkg/app-bkg.png'; // graphcoders-lil-fiber.png
-
-const HeroTitleStyles = css({
-    $$borderColor: '#8e34eb7a', // '#8c01ff7a'
-    $$shadow: `-2px -2px 0 $$borderColor, 2px -2px 0 $$borderColor, -2px 2px 0 $$borderColor, 2px 2px 0 $$borderColor`,
-    color: '#9494e4',
-    textShadow: '$$shadow',
-    'WebkitTextStroke': '1px #8c01ff',
-    'WebkitTextFillColor': '#9494e4'
-});
-
-function Header() {
-    return (
-        <header className="m-8 hidden lg:flex justify-center items-center ">
-
-            <div className="">
-                <div className="">
-
-                    {/* Logo and D3 curves */}
-                    <div className="flex items-center">
-                        <div className="">
-                            <img className="" src={logoImage} alt="logo" />
-                        </div>
-                        <h1 className={`text-[5rem] -ml-4 self-end tracking-tighter font-black ${HeroTitleStyles()}`}>
-                            D3 Curves
-                        </h1>
-                    </div>
-
-                    <div className="ml-16 mb-4 text-base">
-                        <p>Toggle each of the curve types to activate / deactivate the curve. <br className="hidden md:inline" />
-                            You can also add/remove/drag the points to change the shape of the curve.</p>
-                    </div>
-                </div>
-                {/* <HeroAttraction /> */}
-            </div>
-        </header>
-    );
-}
+import { Section1_Header } from './components/Section1_Header';
 
 function Background() {
     return (
@@ -58,7 +19,7 @@ function App() {
             <Background />
             
             <div className="App h-screen flex flex-col items-center text-yellow-900">
-                <Header />
+                <Section1_Header />
 
                 <Section2_Main />
 
--- src/components/Section1_Header.tsx
+++ src/components/Section1_Header.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import { css } from '@stitches/react';
+import HeroAttraction from '@/components/UI/HeroAttraction';
+import logoImage from '@/assets/app-logo.svg';
+
+const HeroTitleStyles = css({
+    $$borderColor: '#8e34eb7a', // '#8c01ff7a'
+    $$shadow: `-2px -2px 0 $$borderColor, 2px -2px 0 $$borderColor, -2px 2px 0 $$borderColor, 2px 2px 0 $$borderColor`,
+    color: '#9494e4',
+    textShadow: '$$shadow',
+    'WebkitTextStroke': '1px #8c01ff',
+    'WebkitTextFillColor': '#9494e4'
+});
 
 export function Section1_Header() {
-  return (
-    <div>Section1_Header</div>
-  )
+    return (
+        <header className="m-8 hidden lg:flex justify-center items-center ">
+
+            <div className="">
+                <div className="">
+
+                    {/* Logo and D3 curves */}
+                    <div className="flex items-center">
+                        <div className="">
+                            <img className="" src={logoImage} alt="logo" />
+                        </div>
+                        <h1 className={`text-[5rem] -ml-4 self-end tracking-tighter font-black ${HeroTitleStyles()}`}>
+                            D3 Curves
+                        </h1>
+                    </div>
+
+                    <div className="ml-16 mb-4 text-base">
+                        <p>Toggle each of the curve types to activate / deactivate the curve. <br className="hidden md:inline" />
+                            You can also add/remove/drag the points to change the shape of the curve.</p>
+                    </div>
+                </div>
+                {/* <HeroAttraction /> */}
+            </div>
+        </header>
+    );
 }
