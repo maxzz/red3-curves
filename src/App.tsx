1e7cb580143dd9ee98941abef1b293719f8bea14
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-05 02:34:11 -0700

    WIP. components split. add Section3_Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Section2_Main } from '@/components/Section2_Main';
-import './App.css';
-// import AppBackground from './assets/bkg/pattern.svg';
-import AppBackground from './assets/bkg/app-bkg.png'; // graphcoders-lil-fiber.png
 import { Section1_Header } from './components/Section1_Header';
+import { Section2_Main } from '@/components/Section2_Main';
+import AppBackground from './assets/bkg/app-bkg.png'; // graphcoders-lil-fiber.png
+//import AppBackground from './assets/bkg/pattern.svg';
+import './App.css';
+import { Section3_Footer } from './components/Section3_Footer';
 
 function Background() {
     return (
@@ -14,26 +15,15 @@ function Background() {
 }
 
 function App() {
-    return (
-        <>
-            <Background />
-            
-            <div className="App h-screen flex flex-col items-center text-yellow-900">
-                <Section1_Header />
+    return (<>
+        <Background />
 
-                <Section2_Main />
-
-                <div className="m-4 mb-12 hidden lg:flex flex-col items-center text-sm">
-                    <span>Created by Max Zakharzhevskiy</span>
-                    <p>Based on <a
-                        className="underline" href="https://bl.ocks.org/d3indepth/raw/b6d4845973089bc1012dec1674d3aff8" target="_blank" rel="noopener">D3 Curve Explorer</a> from <a
-                            className="underline" href="https://www.d3indepth.com/shapes" target="_blank" rel="noopener">D3 in Depth</a> book by Peter Cook
-                    </p>
-                    <span>Open sourced on <a className="underline" href="https://github.com/maxzz/red3-curves" target="_blank" rel="noopener">Github</a></span>
-                </div>
-            </div>
-        </>
-    );
+        <div className="App h-screen flex flex-col items-center text-yellow-900">
+            <Section1_Header />
+            <Section2_Main />
+            <Section3_Footer />
+        </div>
+    </>);
 }
 
 export default App;
